cd0e883845716d536280d23a75d3edfd264d9224
wen8365 <wen8365@gmail.com>
2022-01-15 19:04:51 +0800

    创建一个空工程用于构建和测试流水线

---
+++ src/main/java/com/agilework/sims/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.agilework.sims.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+        return "welcomepage";
+    }
+}
