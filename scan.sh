5e45edbc4f40d1e5fcee293d36ccc6e23716335d
wen8365 <wen8365@gmail.com>
2022-01-22 06:26:11 +0000

    fix merge conflict

--- scan.sh
+++ scan.sh
@@ -1,1 +1,1 @@
-nohup mvn compile sonar:sonar -Dsonar.projectKey=agilework -Dsonar.host.url=http://$1:9000 -Dsonar.login=$2 > scan.log 2>&1 &
+nohup sonar-scanner -Dsonar.login=$1 -Dsonar.projectKey=$2 -Dsonar.projectName=$2 > scan.log 2>&1 &
