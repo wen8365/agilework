bdf48ffcb8720a892ea10c11fcb2803b449d6148
dingle12yy <97778643+dingle12yy@users.noreply.github.com>
2022-02-05 01:42:00 +0800

    添加@NoArgsConstructor和@AllArgsConstructor

--- src/main/java/com/agilework/sims/vo/LoginReq.java
+++ src/main/java/com/agilework/sims/vo/LoginReq.java
@@ -1,9 +1,14 @@
 package com.agilework.sims.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import org.springframework.lang.NonNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginReq {
     @NonNull
     public String userName;
