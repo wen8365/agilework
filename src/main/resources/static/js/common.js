e4b2d9fdcaa05e067c3dd6d88f8b41c540c1d7e6
wen8365 <wen8365@gmail.com>
2022-03-29 16:19:47 +0800

    display local time instead of utc time

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -53,6 +53,16 @@ function getDate(str) {
     }
 }
 
+function formatUTCTime(t) {
+    if (!t) return null;
+    var date = new Date(t);
+    return date.getFullYear() + "-"
+         + (date.getMonth() + 1) + "-"
+         + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + " "
+         + (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":"
+         + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
+}
+
 function handleError(res) {
     console.log(res);
     if (res.status==403) {
